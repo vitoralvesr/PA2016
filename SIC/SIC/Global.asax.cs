96556194e58b498e4d94244f4ee65a0fdeb2738c
danielamarinho <dani.marinho24@gmail.com>
2016-03-10 17:34:30 -0300

    Criação dos projetos de business e data.

--- SIC/SIC/Global.asax.cs
+++ SIC/SIC/Global.asax.cs
@@ -13,7 +13,6 @@ namespace SIC.WebApi
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
