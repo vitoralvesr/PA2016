203775172905bbe5cbac9cda738d99790c3ff48f
danielamarinho <dani.marinho24@gmail.com>
2016-03-24 15:20:41 -0300

    Business

---
+++ SIC/SIC.Business/FrequenciaAlunoBusiness.cs
@@ -0,0 +1,38 @@
+﻿using SIC.Business.Interfaces;
+using SIC.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIC.Business
+{
+    public class FrequenciaAlunoBusiness : IFrequenciaAlunoBusiness
+    {
+        #region [ Propriedades e Atributos ]
+
+        private IFrequenciaAlunoRepository _frequenciaAlunoRepository;
+
+        #endregion
+
+        #region [ Construtor ]
+
+        public FrequenciaAlunoBusiness(IFrequenciaAlunoRepository frequenciaAlunoRepository)
+        {
+            this._frequenciaAlunoRepository = frequenciaAlunoRepository;
+            //teste
+        }
+
+        #endregion
+
+        #region [ Métodos Públicos ]
+
+        public void RealizarChamada()
+        {
+            _frequenciaAlunoRepository.RealizarChamada();
+        }
+
+        #endregion
+    }
+}
